b2cbbd3d906ffb977ab8b6e761e6ce5784d2d627
onb-taller <56447790+onb-taller@users.noreply.github.com>
2019-10-18 13:49:03 -0400

    Add files via upload

---
+++ js/datos.js
@@ -0,0 +1,45 @@
+var data = {	
+promos:[
+{
+"texto_titulo":"Nuevas ofertas",
+"texto_subtitulo":"Los mejores móviles del mercado!",
+"imagen":"telf.jpg",
+"modal":"telf"
+
+},
+{
+    "texto_titulo":"Ofertas de laptops!",
+    "texto_subtitulo":"Las mejores laptops para trabajar!",
+    "imagen":"laptop.jpg",
+    "modal":"laptop"
+},
+{
+    "texto_titulo":"Ofertas de monitores!",
+    "texto_subtitulo":"Monitor de PC de escritorio 19''!",
+    "imagen":"monitor.jpg",
+    "modal":"monitor"
+}
+],
+modales:[
+    {
+    "nombre":"Telefonos",
+    "id":"telf",
+    "titulo":"Ofertas de móviles",
+    "imagen":"telf.jpg",
+    "texto_ampliado":"Ofertamos móviles Samsung y Xiaomi en diferentes tipos. Todos los móviles tienen garantía de 1 mes por escrito."
+    },
+    {
+        "nombre":"Laptop",
+        "id":"laptop",
+        "titulo":"Ofertas de laptops",
+        "imagen":"laptop.jpg",
+        "texto_ampliado":"ASUS A6 AMD, tarjeta de video integrada y memoria RAM 4 GB, 500GB HDD.."
+    },
+    {
+        "nombre":"Monitor",
+        "id":"monitor",
+        "titulo":"Ofertas de monitor",
+        "imagen":"monitor.jpg",
+        "texto_ampliado":"Monitor AOC 19'' LED."
+    }
+    ]};
